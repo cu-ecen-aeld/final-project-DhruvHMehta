ec9a6b89c033d938ff19fdcf9b4c68165066ab9e
Dhruv Mehta <77941477+DhruvHMehta@users.noreply.github.com>
2021-11-11 18:02:55 -0700

    Replace printf with syslog for debug

--- gpio/ultrasonic.c
+++ gpio/ultrasonic.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
 #include <gpiod.h>
 #include <time.h>
 #include <unistd.h>
+#include <syslog.h>
 
 #define GPIOCHIP0   "/dev/gpiochip0"
 #define TRIG        5
@@ -40,12 +40,15 @@ int main()
        gpiod_chip_close(chip);
        return -1;
    }
-
+   
+   syslog(LOG_CRIT, "Requested 0 at trigger");
+   
    rv = gpiod_line_set_value(trigger, 0);
    if(rv != 0)
-       printf("gpiod_line_set_value->1 failed");
+       syslog(LOG_CRIT,"gpiod_line_set_value->1 failed");
 
-
+   syslog(LOG_CRIT, "Set 0 at trigger");
+    
    rv = gpiod_line_request_input(echo, "ultrasonic");
    if(rv != 0)
    {
@@ -60,19 +63,20 @@ int main()
        return -1;
    }
 
+   syslog(LOG_CRIT,"Before sleep 1");
    sleep(1);
-
+   syslog(LOG_CRIT,"After sleep 1");
     while(1)
     {
         rv = gpiod_line_set_value(trigger, 1);
         if(rv != 0)
-            printf("gpiod_line_set_value->1 failed");
+            syslog(LOG_CRIT,"gpiod_line_set_value->1 failed");
 
         usleep(10);
 
         rv = gpiod_line_set_value(trigger, 0);
         if(rv != 0)
-            printf("gpiod_line_set_value->0 failed");
+            syslog(LOG_CRIT,"gpiod_line_set_value->0 failed");
 
         wait_time.tv_sec  = 0;
         wait_time.tv_nsec = 50000000;
@@ -80,28 +84,28 @@ int main()
         rv = gpiod_line_event_wait(echo, NULL);
         if(rv != 1)
         {
-            printf("gpiod_line_event_wait");
+            syslog(LOG_CRIT,"gpiod_line_event_wait");
             gpiod_chip_close(chip);
             return -1;
         }
 
         rv = clock_gettime(CLOCK_MONOTONIC, &start_time);
         if(rv != 0)
-            printf("Something went wrong in clock_gettime\n");
+            syslog(LOG_CRIT,"Something went wrong in clock_gettime\n");
 
         rv = gpiod_line_event_wait(echo, &wait_time);
         if(rv != 1)
         {
-            printf("gpiod_line_event_wait");
+            syslog(LOG_CRIT,"gpiod_line_event_wait");
             gpiod_chip_close(chip);
             return -1;
         }
         
         rv = clock_gettime(CLOCK_MONOTONIC, &end_time);
         if(rv != 0)
-            printf("Something went wrong in clock_gettime\n");
+            syslog(LOG_CRIT,"Something went wrong in clock_gettime\n");
 
-        printf("Distance = %ld\n", V_SOUND*((end_time.tv_nsec)*1000000000));
+        syslog(LOG_CRIT, "Distance = %ld\n", V_SOUND*((end_time.tv_nsec)*1000000000));
 
         sleep(1);        
     }
