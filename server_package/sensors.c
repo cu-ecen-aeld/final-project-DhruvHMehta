02e5b668f4e92846ab34ceb1fc604717b01f3a94
Dhruv Mehta <77941477+DhruvHMehta@users.noreply.github.com>
2021-11-18 16:59:53 -0700

    Add server_package with combined sensors code, cleanup ultrasonic.c

--- server_package/sensors.c
+++ server_package/sensors.c
@@ -1,3 +1,15 @@
+/*****************************************************************
+ *
+ *  Sensors.c
+ *  Brief: Combination of ultrasonic sensor and I2C temperature
+ *         sensor to obtain distance from object and its temperature
+ *
+ *  References: https://olegkutkov.me/2017/08/10/mlx90614-raspberry/
+ *
+ *  Author: Dhruv Mehta
+ *          Rishab Shah
+ *****************************************************************/
+
 #include <gpiod.h>
 #include <time.h>
 #include <unistd.h>
@@ -5,17 +17,55 @@
 #include <errno.h>
 #include <string.h>
 
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <linux/i2c-dev.h>
+#include <linux/i2c.h>
+
+/* Macros for Ultrasonic */
 #define GPIOCHIP0   "/dev/gpiochip0"
 #define TRIG        5
 #define ECHO        6
 #define V_SOUND     330
 
+/* Macros for Temperature */
+#define MLX90614_TA 			(0x06)
+#define MLX90614_TOBJ1 			(0x07)
+#define MLX90614_TOBJ2 			(0x08)
+#define TEMPERATURE_TYPE 		(MLX90614_TOBJ1)
+
+#define MLX90614_DEVICE_ADDRESS		(0x5A)
+#define I2C_DEV_PATH 			("/dev/i2c-1")
+
+#define SLEEP_DURATION 			(1000000)
+
+/* Just in case if these were not defined */
+#ifndef I2C_SMBUS_READ 
+#define I2C_SMBUS_READ 1 
+#endif 
+#ifndef I2C_SMBUS_WRITE 
+#define I2C_SMBUS_WRITE 0 
+#endif
+
+/* Ultrasonic variables for libgpiod pins and time */
 struct gpiod_chip *chip;
 struct gpiod_line *trigger, *echo, *u_power;
 struct timespec start_time, end_time;
 
+/* Temperature i2c_smbus_data struct */
+typedef union i2c_smbus_data i2c_data;
+
 int main()
 {
+
+    /******************************************************
+     *
+     * Ultrasonic Configuration
+     *
+     *****************************************************/
     int rv;
 
     /* Open the gpiochip0 char device driver */
@@ -75,6 +125,43 @@ int main()
 
    /* Let pins stabilize */ 
    sleep(1);
+
+    /*********************************************************
+     *
+     * I2C Temperature Configuration
+     *
+     ********************************************************/
+
+    int fdev = open(I2C_DEV_PATH, O_RDWR); // open i2c bus
+
+    if (fdev < 0) {
+        fprintf(stderr, "Failed to open I2C interface %s Error: %s\n", I2C_DEV_PATH, strerror(errno));
+        return -1;
+    }
+    
+    // set slave device address, default MLX is 0x5A
+    unsigned char i2c_addr = MLX90614_DEVICE_ADDRESS;
+    if (ioctl(fdev, I2C_SLAVE, i2c_addr) < 0) {
+        fprintf(stderr, "Failed to select I2C slave device! Error: %s\n", strerror(errno));
+        return -1;
+    }
+
+    // enable checksums control
+    if (ioctl(fdev, I2C_PEC, 1) < 0) {
+        fprintf(stderr, "Failed to enable SMBus packet error checking, error: %s\n", strerror(errno));
+        return -1;
+    }
+
+    // trying to read something from the device using SMBus READ request
+    i2c_data data;
+    char command = TEMPERATURE_TYPE; 
+    // build request structure
+    struct i2c_smbus_ioctl_data sdat = {
+        .read_write = I2C_SMBUS_READ,
+        .command = command,
+        .size = I2C_SMBUS_WORD_DATA,
+        .data = &data
+    };
    
    while(1)
     {
@@ -122,6 +209,21 @@ int main()
          * Convert nsec to s -> 10^-9 and convert m to cm 10^2 -> Final division -> 10^-7*/
         syslog(LOG_CRIT, "Time difference = %ld\n", end_time.tv_nsec - start_time.tv_nsec);
         syslog(LOG_CRIT, "Distance = %ld\ncm", (V_SOUND*(end_time.tv_nsec - start_time.tv_nsec)/10000000)/2);
+        printf("Distance = %ld\ncm", (V_SOUND*(end_time.tv_nsec - start_time.tv_nsec)/10000000)/2);
+
+        // do actual request
+	    if (ioctl(fdev, I2C_SMBUS, &sdat) < 0) {
+       	fprintf(stderr, "Failed to perform I2C_SMBUS transaction, error: %s\n", strerror(errno));
+        return -1;
+    	}
+	
+	    // calculate temperature in Celsius by formula from datasheet
+	    double temp = (double) data.word;
+    	temp = (temp * 0.02)-0.01;
+    	temp = temp - 273.15;
+
+    	// print result
+    	printf("Temperature value read from object = %04.2f\n", temp);
 
         /* Wait a second before next measurement */
         sleep(1);        
