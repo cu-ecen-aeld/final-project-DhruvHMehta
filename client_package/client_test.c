3b665e391fa960a7037901f0402fdeee4bc02992
Dhruv Mehta <77941477+DhruvHMehta@users.noreply.github.com>
2021-11-20 15:23:31 -0700

    Fix bug in IP address and parameterize IP as argument

--- client_package/client_test.c
+++ client_package/client_test.c
@@ -25,7 +25,7 @@
 #define PORT_NO								(9000)
 #define LED_TOGGLE_TIME				(250*(USEC_TO_MSEC))
 
-#define LOCALTEST 1
+#define GPIOSTATUS 0
 
 //gpio related initialization
 struct gpiod_chip *gpio_driver_fd;
@@ -43,18 +43,29 @@ int data_processing(const char arr[]);
 //client global variables
 int client_socket_fd;
 
-int main()
+int main(int argc, char *argv[])
 {
 	bool dataprocessing_status = false;
 	bool client_init_status = false;
 	char buffer[1024] = {0};
-#if LOCALTEST
+    char IP[20] = {0};
+
+    if(argc != 2)
+    {
+        printf("Usage client_test [IP]\n");
+        return -1;
+    }
+
+    memcpy(IP, argv[1], strlen(argv[1]));
+    printf("Connecting to IP %s\n", IP);
+
+#if GPIOSTATUS
     gpio_init();
 #endif
 	client_init_status = client_init();
 	if(client_init_status == false)
 	{
-		perror("client_socket_fd");  
+		perror("client_init");  
 		return -1;
 	}
 
@@ -83,7 +94,7 @@ bool client_init()
 	server_address.sin_family = AF_INET;
 	server_address.sin_port = htons(PORT_NO);
 	
-	int client_inet_pton_fd = inet_pton(AF_INET,"10.0.247",&server_address.sin_addr);
+	int client_inet_pton_fd = inet_pton(AF_INET,"10.0.0.247",&server_address.sin_addr);
 	if(client_inet_pton_fd <= 0)
 	{
 		perror("client_inet_pton_fd");  
@@ -156,7 +167,7 @@ int data_processing(const char rx_str[])
 void data_reception_indication()
 {
 	LED_status ^= 1;
-#if LOCALTEST
+#if GPIOSTATUS
     gpiod_line_set_value(gpio_LED_line_selected,LED_status);
 #endif	
     printf("LED_STATUS = %d\n",LED_status);
@@ -169,21 +180,21 @@ int gpio_init()
 {
 	int rc = 0;
 	int ret_status = 0;
-
+    printf("init start\n");
 	//get the file handler for gpio driver
 	gpio_driver_fd = gpiod_chip_open("/dev/gpiochip0");
 	if(!gpio_driver_fd)
 	{
 		ret_status = -1; return ret_status; 
 	}
-
+    printf("After chip open\n");
 	//map the required gpio-pin to toggle
 	gpio_LED_line_selected = gpiod_chip_get_line(gpio_driver_fd,GPIO_TOGGLE_PIN);
 	if(!gpio_LED_line_selected)
 	{
 		ret_status = -1; gpiod_chip_close(gpio_driver_fd); return ret_status; 
 	}
-
+    printf("After get line\n");
 	//set the default pin line to one status
 	LED_status = DEFAULT_LED_STATUS;
 	rc = gpiod_line_request_output(gpio_LED_line_selected, "foobar", LED_status); 
@@ -191,7 +202,7 @@ int gpio_init()
 	{
 		ret_status = -1; gpiod_chip_close(gpio_driver_fd); return ret_status; 
 	}
-
+    printf("After request output\n");
 	return ret_status;
 }
 	
